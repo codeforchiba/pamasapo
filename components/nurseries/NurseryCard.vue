c67e46296a3f3396355de3af99498c69603f908f
kazuyukiHATANKAKA <cavy@nifty.com>
2019-09-09 00:17:59 +0900

    eslintの警告修正

--- components/nurseries/NurseryCard.vue
+++ components/nurseries/NurseryCard.vue
@@ -5,8 +5,8 @@
       contain
       aspect-ratio="1"
       max-height="300"
-    ></v-img>
-      <span class="subtitle-1">【{{item.nursery.facility.ownership}}:{{item.nursery.facility.nurseryType}}】</span>
+    />
+    <span class="subtitle-1">【{{ item.nursery.facility.ownership }}:{{ item.nursery.facility.nurseryType }}】</span>
     <v-card-title>
       {{ item.name }}
       <span class="subtitle-1">
@@ -14,15 +14,37 @@
       </span>
     </v-card-title>
     <v-card-text>
-      <span>住所　{{ item.postalCode + item.prefecture + item.city + item.ward + item.address }}</span><br>
-      <span>開園時間　{{ item.nursery.facility.openingTime }}から{{ item.nursery.facility.closingTime }}まで</span><br>
-      <span v-if='item.nursery.facility.numberOfParkingLot>0'>駐車場 あり{{ item.nursery.facility.numberOfParkingLot }}台</span>
-      <span v-else>駐車場 なし</span><br>
-      <span>サービス:夜間休日({{ item.nursery.service.nightCareService | maru_batsu }})</span>
-      <span>一時保育 定期({{ item.nursery.service.temporaryCareService | maru_batsu}})</span>
-      <span>一時保育 不定期({{ item.nursery.service.spotCareService | maru_batsu}})</span>
-      <span>延長({{ item.nursery.service.extendedCareService | maru_batsu}})</span>
-      <span>24時間({{ item.nursery.service.h24CareService | maru_batsu}})</span><br>
+      <span>
+        住所　{{ item.postalCode + item.prefecture + item.city + item.ward + item.address }}
+      </span>
+      <br>
+      <span>
+        開園時間　{{ item.nursery.facility.openingTime }}から{{ item.nursery.facility.closingTime }}まで
+      </span>
+      <br>
+      <span v-if="item.nursery.facility.numberOfParkingLot>0">
+        駐車場 あり{{ item.nursery.facility.numberOfParkingLot }}台
+      </span>
+      <span v-else>
+        駐車場 なし
+      </span>
+      <br>
+      <span>
+        サービス:夜間休日({{ item.nursery.service.nightCareService | maru_batsu }})
+      </span>
+      <span>
+        一時保育 定期({{ item.nursery.service.temporaryCareService | maru_batsu }})
+      </span>
+      <span>
+        一時保育 不定期({{ item.nursery.service.spotCareService | maru_batsu }})
+      </span>
+      <span>
+        延長({{ item.nursery.service.extendedCareService | maru_batsu }})
+      </span>
+      <span>
+        24時間({{ item.nursery.service.h24CareService | maru_batsu }})
+      </span>
+      <br>
     </v-card-text>
     <v-card-actions>
       <v-btn :to="{ name: 'nurseries-id', params: { id: item.id }}">
@@ -38,30 +60,28 @@ import FavoriteButton from "~/components/FavoriteButton"
 
   export default {
     name: 'NurseryCard',
-    components: {FavoriteButton},
-      data() {
-          return {
-              default_image: DefaultImage
-          }
+    components: {
+      FavoriteButton
+    },
+    filters: {
+      maru_batsu: function (value) {
+        if (value) {
+          return '○'
+        } else {
+          return '✕'
+        }
       },
-      filters: {
-        maru_batsu: function (value) {
-          if (value) {
-            return '○'
-          } else {
-            return '✕'
-          }
-        },
-      },
+    },
     props: {
-      item: {}
+      item: {
+        type: Object,
+        required: true
+      }
+    },
+    data() {
+      return {
+          default_image: DefaultImage
+      }
     }
-  },
-
-  data() {
-    return {
-      default_image: DefaultImage
-    }
-  },
-}
+  }
 </script>
