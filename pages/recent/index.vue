02cb241a9d3a0455ce1577e193ddecb66d051d73
hosokawa.takahiro <vwxyzzy@gmail.com>
2019-09-11 21:23:10 +0900

    最近見た園の途中コミット

--- pages/recent/index.vue
+++ pages/recent/index.vue
@@ -1,14 +1,56 @@
 <template>
-  <v-layout>
-    <v-flex text-xs-center>
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;最近みた場所</em>
-          </small>
-        </footer>
-      </blockquote>
-    </v-flex>
-  </v-layout>
+  <v-container grid-list-md text-xs-center>
+    <v-layout row wrap>
+      <v-flex xs12 v-for="item in recent_nursery_items()" :key="item.name">
+        <NurseryCard :item="item.item" />
+        <p class="grey--text lighten-4 float-right">{{item.timestamp}}に閲覧しました</p>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+import { mapGetters } from "vuex";
+import NurseryCard from "~/components/nurseries/NurseryCard.vue";
+
+export default {
+  components: { NurseryCard },
+  data() {
+    return {};
+  },
+
+  computed: {
+    ...mapGetters({
+      nursery_filter_items: "nursery/filter_items",
+      recent_items: "recent/items",
+    })
+  },
+
+  methods: {
+    recent_nursery_items: function() {
+      const items = this.recent_items;
+      const ids = [];
+      items.forEach(function(item) {
+        ids.push(item.id);
+      });
+      const ns = this.nursery_filter_items(ids);
+      const ns_hash = {};
+      ns.forEach((nursery) =>  {
+        ns_hash[nursery.id] = nursery;
+      })
+
+      const nurseries = [];
+      items.forEach(function(item) {
+        let d = new Date(item.timestamp)
+        nurseries.push({item: ns_hash[item.id],timestamp: `${d.toLocaleDateString()} ${`${d.getHours()}時${d.getMinutes()}分`}`});
+      });
+
+      return nurseries;
+    }
+  },
+
+  async fetch({ store }) {
+    await store.dispatch("nursery/search");
+  }
+};
+</script>
---
+++ store/recent.js
@@ -0,0 +1,18 @@
+
+export const state = () => ({
+  items: [],
+  limitLength: 5// context.env.recent.limitLength
+})
+
+export const getters = {
+  items: state => {
+    return state.items
+  },
+}
+
+export const mutations = {
+  add(state, id) {
+    state.items.unshift({ id: id, timestamp: new Date() });
+    state.items = state.items.slice(0, state.limitLength)
+  },
+}
