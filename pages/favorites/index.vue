f89b49cc4c93d3301ccf3704f93410b24eb9a610
URAMOTO Kazunori <uramoto@code4chiba.org>
2019-11-10 23:31:08 +0900

    お気に入り、履歴が 0件のときの表示を追加、タグを小さめに表示できるように。

--- pages/favorites/index.vue
+++ pages/favorites/index.vue
@@ -1,20 +1,33 @@
 <template>
-  <v-container grid-list-md text-xs-center pa-0>
-    <v-list three-line>
-      <template v-for="(item, index) in favorites">
-        <v-divider v-if="index > 0" :key="item.id" />
-        <v-list-item :key="item.id" @click="open(item)">
-          <v-list-item-content>
-            <v-list-item-title>{{ item.name }}</v-list-item-title>
-            <v-list-item-subtitle>{{ item.fullAddress }}</v-list-item-subtitle>
-            <tag-bar :item="item" />
-          </v-list-item-content>
-          <v-list-item-icon>
-            <favorite-button :id="item.id" />
-          </v-list-item-icon>
-        </v-list-item>
-      </template>
-    </v-list>
+  <v-container>
+    <template v-if="hasFavorite">
+      <v-list three-line>
+        <template v-for="(item, index) in favorites">
+          <v-divider v-if="index > 0" :key="item.id" />
+          <v-list-item :key="item.id" @click="open(item)">
+            <v-list-item-content>
+              <v-list-item-title>{{ item.name }}</v-list-item-title>
+              <v-list-item-subtitle>{{ item.fullAddress }}</v-list-item-subtitle>
+              <tag-bar :item="item" :small="true" />
+            </v-list-item-content>
+            <v-list-item-icon>
+              <favorite-button :id="item.id" />
+            </v-list-item-icon>
+          </v-list-item>
+        </template>
+      </v-list>
+    </template>
+    <v-row v-else>
+      <v-col cols="12">
+        <p>
+          気になる保育施設は
+          <v-icon color="pink">
+            favorite
+          </v-icon>
+          を使って、お気に入りに設定しましょう。
+        </p>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -40,6 +53,10 @@ export default {
       centers: "center/items",
       favoriteCenters: "favorite/items"
     }),
+
+    hasFavorite() {
+      return this.favoriteCenters.length > 0
+    },
 
     favorites() {
       return _.filter(this.centers, (c) => this.favoriteCenters.includes(c.id));
--- store/mutation-types.js
+++ store/mutation-types.js
@@ -2,3 +2,4 @@ export const REGISTER_FAVORITE = "favorite/REGISTER";
 export const UNREGISTER_FAVORITE = "favorite/UNREGISTER";
 
 export const ADD_HISTORY = "recent/ADD";
+export const CLEAR_HISTORY = "recent/CLEAR";
