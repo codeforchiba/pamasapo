c39bca1569c151f1a47f6cd889da7dcf967adbf8
snakamu <ping.pong.spanish@gmail.com>
2020-09-21 19:44:56 +0900

    Detail.vue 学童対応

--- components/nurseries/Detail.vue
+++ components/nurseries/Detail.vue
@@ -3,34 +3,38 @@
     <v-list-item>
       <v-list-item-content>
         <v-list-item-subtitle>定員</v-list-item-subtitle>
-        <v-list-item-title>
+        <v-list-item-title v-if="item.nursery">
           3歳未満児(3号) {{ item.nursery.facility.capacity1 }}名 /
           3歳以上児(2号) {{ item.nursery.facility.capacity2 }}名
           <template v-if="item.nursery.facility.capacity3">
             / 教育認定(1号) {{ item.nursery.facility.capacity3 }}名
           </template>
         </v-list-item-title>
+        <v-list-item-title v-if="item.afterSchool">
+          全(低)学年 {{ item.afterSchool.facility.capacityAllGrade }}名 /
+          高学年 {{ item.afterSchool.facility.capacityHighGrade }}名
+        </v-list-item-title>
       </v-list-item-content>
     </v-list-item>
-    <v-list-item v-if="item.nursery.facility.areaOfNurseryRoom">
+    <v-list-item v-if="item.nursery && item.nursery.facility.areaOfNurseryRoom">
       <v-list-item-content>
         <v-list-item-subtitle>保育室の広さ</v-list-item-subtitle>
         <v-list-item-title>{{ item.nursery.facility.areaOfNurseryRoom }}㎡</v-list-item-title>
       </v-list-item-content>
     </v-list-item>
-    <v-list-item v-if="item.nursery.facility.hasYard">
+    <v-list-item v-if="item.nursery && item.nursery.facility.hasYard">
       <v-list-item-content>
         <v-list-item-subtitle>園庭の広さ</v-list-item-subtitle>
         <v-list-item-title>{{ item.nursery.facility.areaOfYard }}</v-list-item-title>
       </v-list-item-content>
     </v-list-item>
-    <v-list-item v-if="item.nursery.facility.hasParkingLot">
+    <v-list-item v-if="item.nursery && item.nursery.facility.hasParkingLot">
       <v-list-item-content>
         <v-list-item-subtitle>駐車場台数</v-list-item-subtitle>
         <v-list-item-title>{{ item.nursery.facility.numberOfParkingLot }}台</v-list-item-title>
       </v-list-item-content>
     </v-list-item>
-    <v-list-item v-if="item.nursery.facility.remarksFacility">
+    <v-list-item v-if="item.nursery && item.nursery.facility.remarksFacility">
       <v-list-item-content>
         <v-list-item-subtitle>備考</v-list-item-subtitle>
         <v-list-item-title>{{ item.nursery.facility.remarksFacility }}</v-list-item-title>
@@ -55,7 +59,7 @@ export default {
     },
 
     facilityAge: function() {
-      const facility = this.item.nursery.facility;
+      const facility = this.item.nursery ? this.item.nursery.facility : "";
       if (facility.age_from === undefined || facility.age_to === undefined) {
         return "";
       }
