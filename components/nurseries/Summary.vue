e3aa804ed7930647c26de10c81caaf2c90bca63d
kazuyukiHATANKAKA <cavy@nifty.com>
2020-09-22 14:29:03 +0900

    項目名不一致の修正

--- components/nurseries/Summary.vue
+++ components/nurseries/Summary.vue
@@ -15,7 +15,7 @@
     <v-list-item v-if="item.nursery && item.nursery.phone">
       <v-list-item-content>
         <v-list-item-subtitle>電話番号</v-list-item-subtitle>
-        <v-list-item-title>{{ item.nursery.phone }}</v-list-item-title>
+        <v-list-item-title>{{ item.phone }}</v-list-item-title>
       </v-list-item-content>
     </v-list-item>
     <v-list-item v-if="item.afterSchool && item.afterSchool.phone">
@@ -24,10 +24,11 @@
         <v-list-item-title>{{ item.afterSchool.phone }}</v-list-item-title>
       </v-list-item-content>
     </v-list-item>
+
     <v-list-item v-if="item.nursery && item.nursery.fax">
       <v-list-item-content>
         <v-list-item-subtitle>FAX番号</v-list-item-subtitle>
-        <v-list-item-title>{{ item.nursery.fax }}</v-list-item-title>
+        <v-list-item-title>{{ item.fax }}</v-list-item-title>
       </v-list-item-content>
     </v-list-item>
     <v-list-item v-if="item.afterSchool && item.afterSchool.fax">
@@ -39,7 +40,7 @@
     <v-list-item v-if="item.nursery && item.nursery.email">
       <v-list-item-content>
         <v-list-item-subtitle>メールアドレス</v-list-item-subtitle>
-        <v-list-item-title>{{ item.nursery.email }}</v-list-item-title>
+        <v-list-item-title>{{ item.email }}</v-list-item-title>
       </v-list-item-content>
     </v-list-item>
     <v-list-item v-if="item.afterSchool && item.afterSchool.email">
@@ -52,8 +53,8 @@
       <v-list-item-content>
         <v-list-item-subtitle>ウェブサイト</v-list-item-subtitle>
         <v-list-item-title>
-          <router-link :to="item.nursery.website" target="_blank">
-            {{ item.nursery.website }}
+          <router-link :to="item.website" target="_blank">
+            {{ item.website }}
           </router-link>
         </v-list-item-title>
       </v-list-item-content>
@@ -74,11 +75,11 @@
         <v-list-item-title>{{ targetAge }}</v-list-item-title>
       </v-list-item-content>
     </v-list-item>
-    <v-list-item v-if="item.remarks_basic">
+    <v-list-item v-if="item.remarks">
       <v-flex xs6 sm3>
         <v-list-item-title>施設基本情報備考</v-list-item-title>
       </v-flex>
-      <v-list-item-content>{{ item.remarks_basic }}</v-list-item-content>
+      <v-list-item-content>{{ item.remarks }}</v-list-item-content>
     </v-list-item>
   </v-list>
 </template>
